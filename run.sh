3992fb26d73e9cc1e14b7418775fce64197b5399
Udaypal Aarkoti <uaarkoti@gmail.com>
2015-07-24 16:14:25 -0700

    Updated docker compose to point to uday's docker images and added run script

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-docker-machine active beedemo-local
+docker-machine start beedemo-local
 eval "$(docker-machine env beedemo-local)"
+docker-machine active
 docker-machine ls
 docker-compose up -d
 docker-compose logs
